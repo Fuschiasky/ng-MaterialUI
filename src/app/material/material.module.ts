bd633a06c34741aabbc16154a19a40f9a80e8b3f
unknown <kevinarnav@gmail.com>
2021-11-12 01:54:23 +0530

    Input

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 
-import {MatStepperModule} from '@angular/material/stepper';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 const MaterialComponents=[
-  MatStepperModule,
-  MatButtonModule
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule
 ]
 
 
