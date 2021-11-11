7f1886b0fe807afd98942ba79feda501c9cedb06
unknown <kevinarnav@gmail.com>
2021-11-12 02:06:02 +0530

    Selection in drop-down

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
 const MaterialComponents=[
   MatButtonModule,
   MatFormFieldModule,
-  MatInputModule
+  MatInputModule,
+  MatSelectModule
 ]
 
 
