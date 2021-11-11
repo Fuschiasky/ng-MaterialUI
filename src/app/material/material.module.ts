16ce59070c46691594f82188078cba4ec6a9509e
unknown <kevinarnav@gmail.com>
2021-11-12 00:53:08 +0530

    Lists

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 
 
 import {MatListModule} from '@angular/material/list';
-
+import {MatIconModule} from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 const MaterialComponents=[
-  MatListModule
+  MatListModule,
+  MatIconModule,
+  MatDividerModule
 ]
 
 
