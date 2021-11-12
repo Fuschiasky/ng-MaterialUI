82ce017c989678854111c66454dcb2e04f7660a8
unknown <kevinarnav@gmail.com>
2021-11-12 07:30:25 +0530

    checkboxes and Radio buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,35 +7,6 @@ import {map, startWith} from 'rxjs/operators';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent implements OnInit{
+export class AppComponent{
   title = 'Material';
-
-  // List of options that need to be displayed as Autocomplete options
-  options: string[] = ['Angular','React','Vue'];
-
-  objectOptions = [
-    {name:'Angular'},
-    {name: 'Angular Material'},
-    {name: 'React'},
-    {name: 'Vue'}
-  ];
- 
-  myControl = new FormControl();
-  filteredOptions!: Observable<string[]>;
-
-  ngOnInit(){
-    this.filteredOptions= this.myControl.valueChanges.pipe(
-      startWith(''),
-      map(value=> this._filter(value))
-    )
-  }
-
-  private _filter(value:string): string[]{
-    const filterValue = value.toLowerCase();
-    return this.options.filter(option => option.toLowerCase().includes(filterValue));
-  }
-
-  displayFn(subject: { name: any; }){
-    return subject? subject.name:undefined;
-  }
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,11 +4,16 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
+
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatRadioModule} from '@angular/material/radio';
 const MaterialComponents=[
   MatButtonModule,
   MatFormFieldModule,
   MatInputModule,
-  MatAutocompleteModule
+  MatAutocompleteModule,
+  MatCheckboxModule,
+  MatRadioModule
   
 ]
 
